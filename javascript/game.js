b15c641dbd4a22736d865907715192284b7503b1
Alexander Militar <militara106@gmail.com>
2019-07-09 00:26:31 -0700

    JS file and game logic finished for character guess

--- javascript/game.js
+++ javascript/game.js
@@ -1,51 +1,99 @@
 // Initial Variables Set
-var guessed = [];
-var wins = document.getElementById("wins");
-var losses = document.getElementById("losses");
-var guessesLeft = document.getElementById("gueesesLeft");
+var guessed;
+var wins;
+var losses;
+var guessesLeft;
+var guessArray = [];
+var charArray = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+// On window load
+window.onload = function () {
+    guessed = document.getElementById("guessed");
+    wins = document.getElementById("wins");
+    losses = document.getElementById("losses");
+    guessesLeft = document.getElementById("guessesLeft");
+    wins.textContent = 0;
+    losses.textContent = 0;
+    guessesLeft.textContent = 9;
+    guessed.textContent = guessArray.toString();
+}
+
+// Initial Scores and text set
 
 // Random number function
 // X is range
-function randomNum(x){
-    var num = Math.floor((Math.random() * (x)) + 1);
+function randomNum(x) {
+    var num = Math.floor((Math.random() * (x)));
     return num;
 }
 
 // Initial Answer Set
-var random = (randomNum(10) - 1);
+var random = charArray[randomNum(26)];
 console.log("Answer is: " + random);
 
 // Compare input to computer
 // X is input
 // Y is computer
-function compare(x, y){
-    if(x==y){
+function compareComp(x, y) {
+    if (x == y) {
+        console.log("compareComp function: true");
         return true;
-    }
-    else{
+    } else {
+        console.log("compareComp function: false");
         return false;
     }
 }
 
+// Check array for guess
+function checkArray(x) {
+    var check = false;
+    for (j = 0; j < guessArray.length; j++) {
+        if (x == guessArray[j]) {
+            check = true;
+        } else {}
+    }
+    console.log("checkArray: " + check);
+    return check;
+}
+
+// Add to guessed array
+function addGuessed(x, y) {
+    if (y == false) {
+        guessArray.push(x);
+        console.log("addGuess:" + guessed.textContent);
+        guessed.textContent = guessArray.toString();
+    } else {}
+}
 
 // Game Logic
 // Start on Key
 document.onkeyup = function (event) {
     // Check if button is a number
-    if (
-        event.key == '1' ||
-        event.key == '2' ||
-        event.key == '3' ||
-        event.key == '4' ||
-        event.key == '5' ||
-        event.key == '6' ||
-        event.key == '7' ||
-        event.key == '8' ||
-        event.key == '9' ||
-        event.key == '0'
-    ) {
+    if (event.keyCode >= 48 && event.keyCode <= 57 || event.keyCode >= 65 && event.keyCode <= 90) {
         // Input Check
-        console.log("Number inputted: " + event.key);
+        console.log("Key Input: " + event.key);
+        if (guessesLeft.textContent > 0) {
+            if (compareComp(event.key, random) == true) {
+                wins.textContent++
+                alert("You win!");
+                addGuessed(event.key, checkArray(event.key));
+                random = charArray[randomNum(26)];
+                guessesLeft.textContent = 9;
+                console.log("Reset: " + wins.textContent + " " + losses.textContent + " " + random);
+            } else {
+                guessesLeft.textContent--
+                addGuessed(event.key, checkArray(event.key));
+                console.log("Wrong Guess")
+            }
+        } else {
+            alert("You lose");
+            losses.textContent++
+            guessed.textContent = "";
+            guessArray = [];
+            random = charArray[randomNum(26)];
+            guessesLeft.textContent = 9;
+            console.log("Reset: " + wins.textContent + " " + losses.textContent + " " + random);
+        }
 
     } else {
         // Input Check
